5f03647d2239dc298bd6b650ad215362f16d4218
Entropy Xu <entropy.xcy@hotmail.com>
2022-12-15 15:16:42 -0500

    Add git ignore and install scripts

--- install_circt.sh
+++ install_circt.sh
@@ -1,18 +1,8 @@
-# CIRCT-SYS
-A rust system-level binding to [circt](https://circt.llvm.org).
-
-**This repository is heavily based on the work of [Moore](https://github.com/fabianschuiki/moore)**
-
-### Installation
-* Clone Circt
-```bash
 git clone https://github.com/llvm/circt
 cd circt
 git checkout sifive/1/24/0
 git submodule update --init --recursive
-```
-* Build LLVM
-```bash
+
 cd llvm
 mkdir build
 cd build
@@ -27,10 +17,7 @@ cmake ../llvm \
     -DLLVM_OPTIMIZED_TABLEGEN=ON \
     -DLLVM_TARGETS_TO_BUILD="host"
 cmake --build . -j
-```
 
-* Build Circt
-```bash
 cd ../../
 mkdir build
 cd build
@@ -40,4 +27,9 @@ cmake .. \
     -DLLVM_DIR=$PWD/../llvm/build/lib/cmake/llvm \
     -DLLVM_ENABLE_ASSERTIONS=ON
 cmake --build . -j
-```
+
+cd ..
+export CIRCT_SYS_CIRCT_DIR=$PWD
+export CIRCT_SYS_CIRCT_BUILD_DIR=$PWD/build
+export CIRCT_SYS_LLVM_DIR=$PWD/llvm
+export CIRCT_SYS_LLVM_BUILD_DIR=$PWD/llvm/build
