46543fc298e05c04353abe91b0002323df64030b
Entropy Xu <entropy.xcy@hotmail.com>
2022-12-15 20:36:50 -0500

    Add test script

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,11 @@
 
 include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
 
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn it_works() {
+        let result = 2 + 2;
+        assert_eq!(result, 4);
+    }
+}
